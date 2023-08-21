b88eb31ef6a0921cdc17b1b59f2eedfcfb357893
Alexander-Guacan <adguacan@espe.edu.ec>
2023-08-21 10:01:54 -0500

    Crear login

--- index.php
+++ index.php
@@ -5,10 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesión</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-
+    <section class="form-wrapper">
+        <form class="form form-login">
+            <fieldset class="form-body form-login_body">
+                <legend class="form-title"><img src="assets/img/Logotipo sin fondo.png" alt="logotype" class="logo"></legend>
+                <div class="form-group">
+                    <label for="input-username" class="label">Usuario</label>
+                    <input type="text" name="input-username" id="input-username" class="input-text">
+                </div>
+                <div class="form-group">
+                    <label for="input-password" class="label">Contraseña</label>
+                    <input type="password" name="input-password" id="input-password" class="input-text">
+                </div>
+            </fieldset>
+            <div class="form-footer">
+                <button type="submit" class="btn">Ingresar</button>
+            </div>
+        </form>
+    </section>
 </body>
 
 </html>
