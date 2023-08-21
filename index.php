283c60827ff4fae04006002abd405e6409d9f1cb
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-21 17:15:07 -0500

    Agregando caja para mensaje de error

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 
-<body>
+<body class="bg-login">
     <section class="form-wrapper">
         <form class="form form-login">
             <fieldset class="form-body form-login_body">
                 <legend class="form-title"><img src="assets/img/Logotipo sin fondo.png" alt="logotype" class="logo"></legend>
                 <div class="form-group">
                     <label for="input-id-card" class="label">Cédula</label>
-                    <input type="text" name="input-id-card" id="input-id-card" class="input-text">
+                    <input type="text" name="input-id-card" id="input-id-card" class="input-text" minlength="10" maxlength="10" placeholder="1703948524">
                 </div>
                 <div class="form-group">
                     <label for="input-password" class="label">Contraseña</label>
@@ -24,9 +24,14 @@
             </fieldset>
             <div class="form-footer">
                 <button type="submit" class="btn">Ingresar</button>
+                <div class="form-msg">
+                    Error: Cédula o contraseña incorrectos
+                </div>
             </div>
         </form>
     </section>
+
+    <script src="js/login.js" type="module"></script>
 </body>
 
 </html>
