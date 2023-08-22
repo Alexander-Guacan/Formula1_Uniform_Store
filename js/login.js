d5ffa90f572df01b6ef3281b4ac764b9ad0143f8
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-22 05:14:57 -0500

    Enviar usuario y contraseña a backend con ajax

--- js/login.js
+++ js/login.js
@@ -1,15 +1,38 @@
 let inputUsername = document.querySelector('#input-username')
 let inputPassword = document.querySelector('#input-password')
+let formLogin = document.querySelector('#form-login')
 
 const validate = {
-    digits: /\d/,
-    lowercase : /[a-z]/,
-    uppercase : /[A-Z]/
+    username: /[a-z0-9]/
 }
 
 inputUsername.addEventListener('keydown', (event) => {
-    let isValidInput = validate.lowercase.test(event.key) || validate.digits.test(event.key) || event.key == 'Backspace'
+    const isValidInput = validate.username.test(event.key) || event.key == 'Backspace'
 
     if (!isValidInput)
         event.preventDefault()
+})
+
+function showErrorMessage() {
+
+}
+
+function login() {
+
+}
+
+formLogin.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    const loginData = Object.fromEntries(new FormData(event.target))
+    const loginString = JSON.stringify(loginData)
+
+    $.ajax({
+        url: 'php/backend/login.php',
+        type: 'POST',
+        data: { login: loginString },
+        success: function (response) {
+            console.log(response)
+        }
+    })
 })
