e907a04cfbc293c37c28fc4fb428cef10cec5759
Alexander-Guacan <adguacan@espe.edu.ec>
2023-08-23 08:30:24 -0500

    agregando informacion de usuario que inicia sesion a sesion php

--- php/backend/login.php
+++ php/backend/login.php
@@ -3,19 +3,35 @@
 
     if (isset($_POST['login'])) {
         $login = json_decode($_POST['login'], true);
-        $query =  "SELECT password FROM users WHERE username = '{$login['input-username']}'";
+        
+        $query =  "SELECT Users.idCard, Users.username, Users.firstName, Users.lastName, UserRoles.name, Users.password
+        FROM Users JOIN UserRoles
+        ON Users.idRol = UserRoles.idRol
+        WHERE username = '{$login['input-username']}'";
+        
         $result = mysqli_query($connection, $query);
+        
         $response_wrong = array(
             'success' => false,
             'result' => 'Usuario o contraseña incorrectos'
         );
 
-        if (!$result->num_rows || !password_verify($login['input-password'], $result->fetch_array()['password']))
+        $userFound = $result->fetch_array();
+
+        if (!$result->num_rows || !password_verify($login['input-password'], $userFound['password']))
             die(json_encode($response_wrong));
+
+        session_start();
+        $_SESSION['user'] = array(
+            'idCard' => $userFound['idCard'],
+            'username' => $userFound['username'],
+            'firstName' => $userFound['firstName'],
+            'lastName' => $userFound['lastName'],
+            'rol' => $userFound['name'],
+        );
         
         echo json_encode(array(
-            'success' => true,
-            'result' => 'Login exitoso'
+            'success' => true
         ));
     }
 
