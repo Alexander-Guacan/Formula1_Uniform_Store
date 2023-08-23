f99522b2bc53a313b0de3c6d859de9bea10322fa
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-23 01:04:29 -0500

    Verificar inicio de sesión

--- php/backend/login.php
+++ php/backend/login.php
@@ -1,13 +1,22 @@
 <?php
+    include_once('./connection.php');
 
     if (isset($_POST['login'])) {
-        return isUser($_POST['login']);
-    }
+        $login = json_decode($_POST['login'], true);
+        $query =  "SELECT password FROM users WHERE username = '{$login['input-username']}'";
+        $result = mysqli_query($connection, $query);
+        $response_wrong = array(
+            'success' => false,
+            'result' => 'Usuario o contraseña incorrectos'
+        );
 
-    function isUser(string $loginJSON) {
-        $login = json_decode($loginJSON, true);
-
-        $query = "SELECT password FROM Users WHERE username = {$login['input-password']}";
+        if (!$result->num_rows || !password_verify($login['input-password'], $result->fetch_array()['password']))
+            die(json_encode($response_wrong));
+        
+        echo json_encode(array(
+            'success' => true,
+            'result' => 'Login exitoso'
+        ));
     }
 
 ?>
