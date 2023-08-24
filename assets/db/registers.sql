f78d3fde89372ab526fd7a28ff05bc1ca44dcf35
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-24 03:32:11 -0500

    Cambiando correo y nombre de usuario para crear registros de prueba

--- assets/db/registers.sql
+++ assets/db/registers.sql
@@ -7,4 +7,4 @@ INSERT INTO userroles (`NAME`) VALUES ('bodega');
 INSERT into users (`IDCARD`, `IDROL`, `FIRSTNAME`, `LASTNAME`, `MOBILENUMBER`, `EMAIL`, `USERNAME`, `PASSWORD`)
 VALUES ('1752937712', 1, 'Alexander', 'Guacán', '0985635691', 'adguacan@espe.edu.ec', 'admin', '$2a$12$k3O7ZJrcef7oxH1IdaOC.u6Ji3hK1L7YBqNRcxLml.A/ZVkCEkRZmoux36WiZI/fIp8kzFNkRrW'); --rootroot
 INSERT into users (`IDCARD`, `IDROL`, `FIRSTNAME`, `LASTNAME`, `MOBILENUMBER`, `EMAIL`, `USERNAME`, `PASSWORD`)
-VALUES ('1711270155', 2, 'Carolina', 'Trejo', '0986237105', 'caro12@gmail.com', 'caro23', '$2a$12$J0Fl4ft/tob1S0yK0OzF2.u37jUiyNqZ5AmTc0vb2f1WBwVq.TE7C'); --caro780
+VALUES ('1711270155', 2, 'Carolina', 'Trejo', '0986237105', 'carol.trejo@gmail.com', 'carol123', '$2a$12$J0Fl4ft/tob1S0yK0OzF2.u37jUiyNqZ5AmTc0vb2f1WBwVq.TE7C'); --caro780
