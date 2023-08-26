b1dedacc7fbdc19775b2b0ed72c7dfcd955cf800
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-26 14:51:40 -0500

    Corregir ubicacion del valor del campo password

--- assets/db/registers.sql
+++ assets/db/registers.sql
@@ -12,9 +12,8 @@ INSERT INTO measure (name) VALUES
 ('unidades');
 
 /* Users */
-INSERT INTO users (idCard, idRol, firstName, lastName, mobileNumber, email, username, password)
-VALUES
+INSERT INTO users (idCard, idRol, firstName, lastName, mobileNumber, email, username, password) VALUES
 ('1752937712', 1, 'Alexander', 'Guacán', '0985635691', 'adguacan@espe.edu.ec', 'admin', '$2a$12$k3O7ZJrcef7oxH1IdaOC.u6Ji3hK1L7YBqNRcxLml.A/ZVkCEkRZmoux36WiZI/fIp8kzFNkRrW'),
 ('1711270155', 2, 'Carolina', 'Trejo', '0986237105', 'carol.trejo@gmail.com', 'carol123', '$2a$12$y/wSQLp9vjNb6keQ8KDo4u.dvuP2LSZ5ALOE/GblvtfrqtOBGVyou'),
 ('1717774788', 3, 'Edgar', 'Fernandez', '0972439839', 'edgar.fernandez@gmail.com', 'edfernan', '$2a$12$nPo6/xaSMKKqSaECk6D.Guxk8tvz2SIaIk/sCErT06eocEvpZhqT.'),
-('1799234123', 2, 'Alejandra', 'Palacios', 'alepalacios', '$2a$12$aELcWKPu6uKHZtXFcRZuy.Gg6AH8ELdMe6unu6rrirMrNns.ah00G', '0925474343', 'alepalacios@gmail.com');
+('1799234123', 2, 'Alejandra', 'Palacios', '0925474343', 'alepalacios@gmail.com', 'alepalacios', '$2a$12$aELcWKPu6uKHZtXFcRZuy.Gg6AH8ELdMe6unu6rrirMrNns.ah00G');
