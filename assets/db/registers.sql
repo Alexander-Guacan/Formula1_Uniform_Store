a8dde379b6e67b7d18b3e211f6bbedb0a56df5fa
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-25 19:13:17 -0500

    Cargar nuevo usuario de ventas

--- assets/db/registers.sql
+++ assets/db/registers.sql
@@ -1,15 +1,13 @@
 -- Roles
-INSERT INTO userroles (`NAME`) VALUES ('admin');
-INSERT INTO userroles (`NAME`) VALUES ('ventas');
-INSERT INTO userroles (`NAME`) VALUES ('bodega');
+INSERT INTO userroles (name) VALUES ('admin');
+INSERT INTO userroles (name) VALUES ('ventas');
+INSERT INTO userroles (name) VALUES ('bodega');
 
 -- Users
- --rootroot
-INSERT into users (`IDCARD`, `IDROL`, `FIRSTNAME`, `LASTNAME`, `MOBILENUMBER`, `EMAIL`, `USERNAME`, `PASSWORD`)
+INSERT into users (idCard, idRol, firstName, lastName, mobileNumber, email, username, password)
 VALUES ('1752937712', 1, 'Alexander', 'Guacán', '0985635691', 'adguacan@espe.edu.ec', 'admin', '$2a$12$k3O7ZJrcef7oxH1IdaOC.u6Ji3hK1L7YBqNRcxLml.A/ZVkCEkRZmoux36WiZI/fIp8kzFNkRrW');
---caro780
-INSERT into users (`IDCARD`, `IDROL`, `FIRSTNAME`, `LASTNAME`, `MOBILENUMBER`, `EMAIL`, `USERNAME`, `PASSWORD`)
-VALUES ('1711270155', 2, 'Carolina', 'Trejo', '0986237105', 'carol.trejo@gmail.com', 'carol123', '$2a$12$J0Fl4ft/tob1S0yK0OzF2.u37jUiyNqZ5AmTc0vb2f1WBwVq.TE7C');
---edfernan
-INSERT into users (`IDCARD`, `IDROL`, `FIRSTNAME`, `LASTNAME`, `MOBILENUMBER`, `EMAIL`, `USERNAME`, `PASSWORD`)
+INSERT into users (idCard, idRol, firstName, lastName, mobileNumber, email, username, password)
+VALUES ('1711270155', 2, 'Carolina', 'Trejo', '0986237105', 'carol.trejo@gmail.com', 'carol123', '$2a$12$y/wSQLp9vjNb6keQ8KDo4u.dvuP2LSZ5ALOE/GblvtfrqtOBGVyou');
+INSERT into users (idCard, idRol, firstName, lastName, mobileNumber, email, username, password)
 VALUES ('1717774788', 3, 'Edgar', 'Fernandez', '0972439839', 'edgar.fernandez@gmail.com', 'edfernan', '$2a$12$nPo6/xaSMKKqSaECk6D.Guxk8tvz2SIaIk/sCErT06eocEvpZhqT.');
+INSERT INTO users(idCard, idRol, firstName, lastName, username, password, mobileNumber, email) VALUES('1799234123', 2, 'Alejandra', 'Palacios', 'alepalacios', '$2a$12$aELcWKPu6uKHZtXFcRZuy.Gg6AH8ELdMe6unu6rrirMrNns.ah00G', '0925474343', 'alepalacios@gmail.com');
