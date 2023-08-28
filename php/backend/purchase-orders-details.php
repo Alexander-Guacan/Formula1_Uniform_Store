6fa356ad387d34627efafb7ad3a783418ead4a8e
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-27 20:20:52 -0500

    Extraer los detalles de una orden de compra especifica

---
+++ php/backend/purchase-orders-details.php
@@ -0,0 +1,32 @@
+<?php
+
+    include_once('./connection.php');
+
+    if (isset($_GET['read'])) {
+        $idPurchaseOrder = $_GET['idPurchaseOrder'];
+        $query = "SELECT Items.idItem, Items.name AS name, Items.price, PurchaseOrdersDetails.quantityItemPurchased, Measures.name AS measure
+        FROM PurchaseOrdersDetails
+        JOIN Items
+        ON Items.idItem = PurchaseOrdersDetails.idItem
+        JOIN measures
+        ON Items.idMeasure = Measures.idMeasure
+        WHERE idPurchaseOrder = '$idPurchaseOrder'";
+
+        $response = $connection->query($query);
+
+        $json = array();
+
+        while ($row = $response->fetch_array()) {
+            $json[] = array(
+                'idItem' => $row['idItem'],
+                'name' => $row['name'],
+                'price' => $row['price'],
+                'quantity' => $row['quantityItemPurchased'],
+                'measure' => $row['measure'],
+            );
+        }
+
+        echo json_encode($json);
+    }
+
+?>
