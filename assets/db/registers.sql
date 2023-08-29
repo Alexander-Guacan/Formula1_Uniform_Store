642741f7ec75cd2002a89e0731d364ca8af2fb8a
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-28 20:29:56 -0500

    Cambiar rol a admin

--- assets/db/registers.sql
+++ assets/db/registers.sql
@@ -21,4 +21,4 @@ INSERT INTO measures (name) VALUES
 
 /* Users */
 INSERT INTO users (idCard, idRol, firstName, lastName, mobileNumber, email, username, password) VALUES
-('1752937712', 1, 'Alexander', 'Guacán', '0985635691', 'adguacan@espe.edu.ec', 'admin', '$2a$12$k3O7ZJrcef7oxH1IdaOC.u6Ji3hK1L7YBqNRcxLml.A/ZVkCEkRZmoux36WiZI/fIp8kzFNkRrW');
+('1752937712', 2, 'Alexander', 'Guacán', '0985635691', 'adguacan@espe.edu.ec', 'admin', '$2a$12$k3O7ZJrcef7oxH1IdaOC.u6Ji3hK1L7YBqNRcxLml.A/ZVkCEkRZmoux36WiZI/fIp8kzFNkRrW');
