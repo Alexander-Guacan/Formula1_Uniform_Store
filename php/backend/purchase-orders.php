a9614d0beca88f9d1418b74cad0dc328d8760551
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-28 22:59:18 -0500

    Extraer nombre y apellido del responsable de la orden de compra

--- php/backend/purchase-orders.php
+++ php/backend/purchase-orders.php
@@ -3,8 +3,10 @@
     include_once('./connection.php');
 
     if (isset($_GET['read'])) {
-        $query = "SELECT *
-        FROM PurchaseOrders";
+        $query = "SELECT PurchaseOrders.idPurchaseOrder, PurchaseOrders.date,
+        Users.idCard, Users.firstName, Users.lastName
+        FROM PurchaseOrders JOIN Users
+        ON PurchaseOrders.idCard = Users.idCard";
 
         $response = $connection->query($query);
 
@@ -14,6 +16,8 @@
             $json[] = array(
                 'id' => $row['idPurchaseOrder'],
                 'idCard' => $row['idCard'],
+                'firstName' => $row['firstName'],
+                'lastName' => $row['lastName'],
                 'date' => $row['date']
             );
         }
