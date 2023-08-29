cecec7a0926ac86b57ed56959ee9c461e1bcf50f
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-29 16:23:44 -0500

    Agregar formulario popup para editar producto

--- php/view/products.php
+++ php/view/products.php
@@ -57,6 +57,31 @@ if ($_SESSION['user']['rol'] != 'ventas')
     </table>
 </div>
 
+<form class="popup" id="form-edit-product">
+    <a class="icon popup_btn-close" href="#"><i class="fa-solid fa-xmark"></i></a>
+    <fieldset class="form-body form-login_body">
+        <legend class="form-title">
+            <h2>Editar producto</h2>
+        </legend>
+        <div class="form-group">
+            <label for="input-edit-id" class="label">Id</label>
+            <input type="text" name="input-edit-id" id="input-edit-id" class="input-text" disabled>
+        </div>
+        <div class="form-group colspan-2">
+            <label for="textarea-edit-name" class="label">Nombre</label>
+            <textarea name="textarea-edit-name" id="textarea-edit-name" cols="30" rows="10" class="textarea"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="select-edit-products-size" class="label">Talla</label>
+            <select name="select-edit-products-size" id="select-edit-products-size" class="select"></select>
+        </div>
+    </fieldset>
+    <div class="form-footer">
+        <button type="submit" class="btn">Guardar</button>
+        <div class="informative-msg">Mensaje de error</div>
+    </div>
+</form>
+
 <script src="../../js/products.js"></script>
 
 <?php
