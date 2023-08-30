cd019997e411ab6984644d28e7b414f831c0ac89
AlexanderGuacan <alexander.guacan2003@gmail.com>
2023-08-29 23:57:34 -0500

    Agregar boton para generar repore PDF

--- php/view/purchase-orders.php
+++ php/view/purchase-orders.php
@@ -45,9 +45,15 @@ if ($_SESSION['user']['rol'] != 'bodega')
             </tr>
         </tfoot>
     </table>
+    <div class="table-actions">
+        <div class="table-actions_left"></div>
+        <div class="table-actions_right">
+            <button type="button" class="btn state-wrong" id="btn-create-pdf"><i class="fa-solid fa-file"></i>&emsp;PDF</button>
+        </div>
+    </div>
 </div>
 
- <script src="../../js/purchase-orders.js"></script>
+<script src="../../js/purchase-orders.js"></script>
 
 <?php
 include('./control-panel-footer.php');
